fcaad1ded5a64e411b88ebe8924f01172df960d2
parameterized <troyk212@gmail.com>
2017-05-06 23:15:29 -0400

    revert this

--- README.md
+++
@@ -1,2 +0,0 @@
-# deluge
-2D Pixel Brawler Game developed using Love2D in Lua.
--- conf.lua
+++
@@ -1,9 +0,0 @@
-
-function love.conf(t)
-	t.title = "Deluge"
-	t.window.width = 480*3
-	t.window.height = 270*3
-	t.window.resizable = true
-	t.window.vsync = true
-	t.console = true
-end
